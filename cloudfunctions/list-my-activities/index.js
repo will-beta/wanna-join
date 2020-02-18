de54a4cdb0ed1f4fe86b6064c8d3371f70139a86
will-beta <i@will-beta.com>
2020-02-19 00:10:34 +0800

    实现查询

--- cloudfunctions/list-my-activities/index.js
+++ cloudfunctions/list-my-activities/index.js
@@ -7,56 +7,13 @@ cloud.init()
 exports.main = async(event, context) => {
   const wxContext = cloud.getWXContext()
 
-  /*
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
-  */
+  const records = await cloud
+    .database()
+    .collection('my-activities')
+    .where({
+      _openid: wxContext.OPENID
+    })
+    .get()
 
-  let myActivities = [{
-    coverImage: "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg",
-    title: "活动0",
-    publisher: "大贝塔",
-    startTime: "2020-1-0",
-    location: {
-      name: "海南",
-      address: null
-    },
-    status: "已报名"
-  }, {
-    coverImage: "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg",
-    title: "活动1",
-    publisher: "大贝塔",
-    startTime: "2020-1-1",
-    location: {
-      name: "浙江",
-      address: null
-    },
-    status: "已报名"
-  }, {
-    coverImage: "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg",
-    title: "活动2",
-    publisher: "大贝塔",
-    startTime: "2020-1-2",
-    location: {
-      name: "上海",
-      address: null
-    },
-    status: "已报名"
-  }, {
-    coverImage: "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg",
-    title: "活动3",
-    publisher: "大贝塔",
-    startTime: "2020-1-3",
-    location: {
-      name: "北京",
-      address: null
-    },
-    status: "已报名"
-  }]
-
-  return myActivities
+  return records.data
 }
