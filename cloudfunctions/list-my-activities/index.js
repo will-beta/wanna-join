2a137b95acbc90a9fd7beaf0f84b3c63941469ef
will-beta <i@will-beta.com>
2020-02-20 00:35:30 +0800

    完善数据库设计

--- cloudfunctions/list-my-activities/index.js
+++ cloudfunctions/list-my-activities/index.js
@@ -6,14 +6,29 @@ cloud.init()
 // 云函数入口函数
 exports.main = async(event, context) => {
   const wxContext = cloud.getWXContext()
+  const db = cloud.database()
 
-  const records = await cloud
-    .database()
+  const myActivities = await db
     .collection('my-activities')
     .where({
       _openid: wxContext.OPENID
     })
     .get()
+  console.log("myActivities")
+  console.log(myActivities)
 
-  return records.data
+  const myActivityIds = myActivities.data.map(d => d.activityId)
+  console.log("myActivityIds")
+  console.log(myActivityIds)
+
+  const entities = await db
+    .collection('activities')
+    .where({
+      _id: db.command.in(myActivityIds),
+    })
+    .get()
+  console.log("entities")
+  console.log(entities)
+
+  return entities.data
 }
